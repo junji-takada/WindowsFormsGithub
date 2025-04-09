97dcdc95da84fcf4ca70aa1dd048fff45ca1127a
jt <junjitakada@outlook.jp>
2025-04-09 16:25:50 +0900

    Install-Package EntityFramework EFSampleContext追加

---
+++ EFSampleContext.cs
@@ -0,0 +1,58 @@
+﻿//https://qiita.com/ymasaoka/items/944e8a5f1987cc9e0d37
+//User
+//Task
+//EFSampleContext
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace WindowsFormsGithub
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public virtual IList<Task> Tasks { get; set; }
+
+        public String GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public override string ToString()
+        {
+            return "User [id=" + this.UserId + ", name=" + this.GetFullName() + "]";
+        }
+    }
+
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsComplete { get; set; }
+        public virtual User AssignedTo { get; set; }
+
+        public override string ToString()
+        {
+            return "Task [id=" + this.TaskId + ", title=" + this.Title + ", dueDate=" + this.DueDate.ToString() + ", IsComplete=" + this.IsComplete + "]";
+        }
+    }
+
+    class EFSampleContext : DbContext
+    {
+        public EFSampleContext(string connectionString)
+        {
+            Database.SetInitializer<EFSampleContext>(new DropCreateDatabaseAlways<EFSampleContext>());
+            this.Database.Connection.ConnectionString = connectionString;
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
--- SqlEntityf.Designer.cs
+++ SqlEntityf.Designer.cs
@@ -29,12 +29,104 @@ namespace WindowsFormsGithub
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.textConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(49, 25);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(75, 23);
+            this.btnAll.TabIndex = 0;
+            this.btnAll.Text = "All";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(49, 71);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(49, 116);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(49, 166);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(49, 211);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(49, 259);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // textConsole
+            // 
+            this.textConsole.Location = new System.Drawing.Point(297, 28);
+            this.textConsole.Name = "textConsole";
+            this.textConsole.Size = new System.Drawing.Size(385, 19);
+            this.textConsole.TabIndex = 6;
+            // 
+            // SqlEntityf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textConsole);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnAll);
+            this.Name = "SqlEntityf";
             this.Text = "SqlEntityf";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox textConsole;
     }
 }
